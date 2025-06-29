0b4cd76bf3bea38060a6f9864e38d8821248755c
Xinsheng Ooi <84843079+xs1128@users.noreply.github.com>
2025-06-29 17:04:55 +0800

    Clean up code

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,9 +1,8 @@
 def ceaser_cipher(target, shifts = 1)
-  arr = target.chars.map { |c| c.ord }
-  arr.each do |c|
-    if c >= 'A'.ord && c <= 'Z'.ord
+  arr = target.chars.map do |c|
+    if c >= 'A' && c <= 'Z'
       print(((c.ord + shifts - 'A'.ord) % 26 + 'A'.ord).chr)
-    elsif c >= 'a'.ord && c <= 'z'.ord
+    elsif c >= 'a' && c <= 'z'
       print(((c.ord + shifts - 'a'.ord) % 26 + 'a'.ord).chr)
     else
       print c.chr
