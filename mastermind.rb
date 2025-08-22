a21778f416777098a6c663e05120e240b3e34c88
Xinsheng Ooi <ooixinsheng@gmail.com>
2025-08-22 12:45:55 +0800

    feat: mastermind game

--- mastermind.rb
+++ mastermind.rb
@@ -1,63 +1,52 @@
 # frozen_string_literal: true
 
 tries = 12
-puts 'Do you want to create the secret code or guess mine? (Create: 0, Guess: 1)'
-response = gets.to_i
-
 secret_code = Array.new(4, -1)
 
-if response.zero?
+puts 'I am making a code that you\'ll never guess...'
 
-else
-  puts 'I am making a code that you\'ll never guess...'
+# Create random string of 4
+(0..3).each do |i|
+  secret_code[i] = rand(1..7)
+end
 
-  # Create random string of 4
-  (0..3).each do |i|
-    secret_code[i] = rand(1..7)
+player_guess = Array.new(4, -1)
+until tries.zero?
+  # guess
+  puts 'Make your first guess!' if tries == 12
+
+  guess = gets
+  3.downto(0) do |i|
+    player_guess[i] = guess[i].to_i
   end
 
-  player_guess = Array.new(4, -1)
-  until tries.zero?
-    # guess
-    puts 'Make your first guess!' if tries == 12
+  correct_all = correct_color = 0
 
-    guess = gets
-    3.downto(0) do |i|
-      player_guess[i] = guess[i].to_i
-    end
-
-    # loop through the correc placement
-    matched_secret = Array.new(4, false)
-    matched_guess = Array.new(4, false)
-
-    correct_place = correct_color = 0
-
-    (0..3).each do |i|
-      next unless player_guess[i] == secret_code[i]
-
-      correct_place += 1
-      matched_guess[i] = true
-      matched_secret[i] = true
-    end
-
-    (0..3).each do |i|
-      next if matched_guess[i]
-
-      (0..3).each do |j|
-        next if matched_secret[j]
-
-        next unless player_guess[i] == matched_secret[j]
-
-        correct_color += 1
-        matched_secret[j] = true
-        break
-      end
-    end
-
-    # check how color correct
-    puts "#{correct_color} correct colors and #{correct_place} correct placements!"
-    tries -= 1
+  (0...4).each do |i|
+    correct_all += 1 if player_guess[i] == secret_code[i]
   end
+
+  player_colors = Hash.new(0)
+  secret_colors = Hash.new(0)
+
+  (0...4).each do |i|
+    next if player_guess[i] == secret_code[i]
+
+    player_colors[player_guess[i]] += 1
+    secret_colors[secret_code[i]] += 1
+  end
+
+  player_colors.each do |color, cnt|
+    correct_color += [cnt, secret_colors[color]].min
+  end
+
+  puts "#{correct_all} correct colors and correct position."
+  puts "#{correct_color} correct colors but incorrect position."
+
+  break if correct_all == 4
+
+  tries -= 1
 end
 
 puts 'Too bad, you ran out of tries!' if tries.zero?
+puts 'Congratz on guessing all!' unless tries.zero?
